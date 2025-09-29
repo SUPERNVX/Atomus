31741a61481f8cb6f5ba4a7c98110058a106f2dc
SUPERNVX <supernvxofc@gmail.com>
2025-09-29 16:05:26 -0300

    Translation fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // For GitHub Pages deployment
+  base: '/Atomus/', // For GitHub Pages deployment
   server: {
     port: 3000,
     host: '0.0.0.0',
